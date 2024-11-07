562ef78300ce73c389f4952dd70581067a9d9e48
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-07 16:38:04 -0300

    feat: add axios interceptor to refresh authentication token

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import { useAuth } from '@/hooks/use-auth';
+import { api } from '@/services/api';
+import { useEffect } from 'react';
 
 export default function Dashboard() {
 	const { user } = useAuth();
+
+	useEffect(() => {
+		api.get('/me').then((response) => console.log(response));
+	}, []);
 
 	return <h1>Dashboard: {user?.email}</h1>;
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { type AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+type AxiosErrorResponse = {
+	code?: string;
+};
+
+let cookies = parseCookies();
 
 export const api = axios.create({
 	baseURL: 'http://localhost:3333',
@@ -9,3 +13,33 @@ export const api = axios.create({
 		Authorization: `Bearer ${cookies['ignite-reactjs-auth-jwt-app.token']}`
 	}
 });
+
+api.interceptors.response.use(
+	(response) => response,
+	(error: AxiosError<AxiosErrorResponse>) => {
+		if (error.response.status === 401) {
+			if (error.response.data?.code === 'token.expired') {
+				cookies = parseCookies();
+
+				const { 'ignite-reactjs-auth-jwt-app.refreshToken': refreshToken } = cookies;
+
+				api.post('/refresh', { refreshToken }).then((response) => {
+					const { token } = response.data;
+
+					setCookie(undefined, 'ignite-reactjs-auth-jwt-app.token', token, {
+						maxAge: 60 * 60 * 24 * 30, // 30 days
+						path: '/'
+					});
+					setCookie(undefined, 'ignite-reactjs-auth-jwt-app.refreshToken', response.data.refreshToken, {
+						maxAge: 60 * 60 * 24 * 30, // 30 days
+						path: '/'
+					});
+
+					api.defaults.headers.Authorization = `Bearer ${token}`;
+				});
+			} else {
+				// logout
+			}
+		}
+	}
+);
