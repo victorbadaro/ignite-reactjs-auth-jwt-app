fef6a4bf14c37ee6313fab2f8bc77ac43c5adbd2
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-07 18:22:58 -0300

    feat: add a catch to log errors in the dashboard page initial request

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,7 +6,10 @@ export default function Dashboard() {
 	const { user } = useAuth();
 
 	useEffect(() => {
-		api.get('/me').then((response) => console.log(response));
+		api
+			.get('/me')
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error));
 	}, []);
 
 	return <h1>Dashboard: {user?.email}</h1>;
