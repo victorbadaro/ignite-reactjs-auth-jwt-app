6a2d0641841f89825bb5f7f0319b40764fef0774
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-11 19:15:43 -0300

    feat: add server side permissions validation

--- src/utils/with-ssr-auth.ts
+++ src/utils/with-ssr-auth.ts
@@ -1,18 +1,46 @@
 import { AuthTokenError } from '@/services/errors/auth-token-error';
+import decode from 'jwt-decode';
 import type { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { destroyCookie, parseCookies } from 'nookies';
+import { validateUserPermissions } from './validate-user-permissions';
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type WithSSRAuthOptions = {
+	permissions?: string[];
+	roles?: string[];
+};
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions): GetServerSideProps {
 	return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
 		const cookies = parseCookies(ctx);
+		const token = cookies['ignite-reactjs-auth-jwt-app.token'];
 
-		if (!cookies['ignite-reactjs-auth-jwt-app.token']) {
+		if (!token) {
 			return {
 				redirect: {
 					destination: '/',
 					permanent: false
 				}
 			};
+		}
+
+		if (options) {
+			const user = decode<{ permissions: string[]; roles: string[] }>(token);
+			const { permissions, roles } = options;
+
+			const userHasValidPermissions = validateUserPermissions({
+				user,
+				permissions,
+				roles
+			});
+
+			if (!userHasValidPermissions) {
+				return {
+					redirect: {
+						destination: '/dashboard',
+						permanent: false
+					}
+				};
+			}
 		}
 
 		try {
